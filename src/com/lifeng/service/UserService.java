d8ae96614bf589d0d74ab0378ad201e14fc185bd
mi991203 <1101315853@qq.com>
2020-06-27 10:46:06 +0800

    这是测试完成后的第一次提交

---
+++ src/com/lifeng/service/UserService.java
@@ -0,0 +1,13 @@
+package com.lifeng.service;
+
+import java.util.List;
+
+import com.lifeng.entity.User;
+
+
+public interface UserService {
+	
+	// ÓÃ»§µÇÂ¼
+	public User login(String username, String password);
+	
+}
